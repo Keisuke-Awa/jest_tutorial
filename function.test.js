bf0302fe60d23c54bc3bb0c57fa3bc392cf53a1f
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2023-07-19 11:15:07 +0900

    テストの調整

--- function.test.js
+++ function.test.js
@@ -4,7 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
 })
 
-// 2が偶数化を判定するtest
+// even関数のtest
 test('judgle 2 is even? to be true', () => {
-    expect(even(2)).toBeTruthy()
+  // 2が偶数化を判定する
 })
